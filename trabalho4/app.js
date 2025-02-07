6be844a57740ccbf710d6240a79fff22c47d13b5
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2025-02-07 15:47:12 -0300

    Add files via upload

--- trabalho4/app.js
+++ trabalho4/app.js
@@ -14,3 +14,13 @@ function loadFile(container,file){
 }
 loadFile('container-sidebar','sidebar.html');
 loadFile('container-navbar','navbar.html');
+
+setTimeout(()=>{
+    const toggleMenu = document.getElementById('toggleMenu');
+    let sidebar = document.querySelector('.sidebar');
+    toggleMenu.addEventListener('click',()=>{
+
+        sidebar.classList.toggle('sidebar-show');
+
+    });
+},1500);
